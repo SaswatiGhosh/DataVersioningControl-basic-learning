362326bfbd72bb201f91fa5b3efee0b477b7f328
SaswatiGhosh <saswatititli99@gmail.com>
2025-05-26 07:56:26 +0530

    second version updated

--- mycode.py
+++ mycode.py
@@ -10,11 +10,14 @@ data={'Name': ['Alice', 'Bob', 'Charlie'],
 df = pd.DataFrame(data)
 print(df)
 
+new_row_loc ={'Name': 'GF1', 'Age': '19', 'City' : 'NewYork' }
+df.loc[len(df.index)]= new_row_loc
+
 data_dir = 'data'
 os.makedirs(data_dir , exist_ok=True)
 
 file_path = os.path.join(data_dir,"samples.csv")
-
+print(df)
 df.to_csv(file_path, index=False)
 
 print(f"CSV file is save to {file_path}")
